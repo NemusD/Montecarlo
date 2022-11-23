33b29c5d267db69e0e842c20222ac2574c69fc6f
Christian Droguett <ch.droguett.l@gmail.com>
2022-11-23 13:01:43 -0300

    actualización de la database

--- db/database.sql
+++ db/database.sql
@@ -4,10 +4,9 @@ Use QADdatabase;
 
 CREATE TABLE employee (
     id INT(11) NOT NULL AUTO_INCREMENT,
-    name Varchar (45) DEFAULT NULL
-    salary INT, DEFAULT NULL,
-    PRIMARY KEY (id)
-    
+    name Varchar (45) DEFAULT NULL,
+    salary INT (5), DEFAULT NULL,
+    PRIMARY KEY (id),    
 );
 
 DESCRIBE employee;
@@ -16,4 +15,4 @@ INSERT INTO employee VALUES
 (1, 'Joe', 1000),
 (2, 'Henry', 2500),
 (3, 'Sam', 2000),
-(4, 'Max', 1500;)
+(4, 'Max', 1500);
